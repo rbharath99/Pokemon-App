90c013c26c122eb54193898d10e9f589f513ac6a
Ryan Bharath <dbossman60@gmail.com>
2022-11-26 01:12:47 -0500

    refactor: added try catch block in api call

--- lib/repository/pokemon_repository.dart
+++ lib/repository/pokemon_repository.dart
@@ -8,13 +8,15 @@ class PokemonRepository {
       'https://raw.githubusercontent.com/rsr-itminds/flutter-workshop/master/data/pokedex.json';
 
   Future<List<Pokemon>> fetchPokemonData() async {
-    final response = await http.get(Uri.parse(baseUrl));
-
-    final List<dynamic> data = jsonDecode(response.body);
-
-    final List<Pokemon> pokemons =
-        data.map((item) => Pokemon.fromJson(item)).toList();
-
-    return pokemons;
+    try {
+      final response = await http.get(Uri.parse(baseUrl));
+      final List<dynamic> data = jsonDecode(response.body);
+      final List<Pokemon> pokemons =
+          data.map((item) => Pokemon.fromJson(item)).toList();
+      return pokemons;
+    } catch (e) {
+      print('Data could not be fetched -> $e');
+      return [];
+    }
   }
 }
