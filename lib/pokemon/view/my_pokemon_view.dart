918d59218306585e5d5409c08a6e10037cd8e6ea
Ryan Bharath <dbossman60@gmail.com>
2022-12-01 23:11:30 -0500

    chore: worked on styling for the my pokemons list view

--- lib/pokemon/view/my_pokemon_view.dart
+++ lib/pokemon/view/my_pokemon_view.dart
@@ -12,22 +12,35 @@ class MyPokemonListView extends StatelessWidget {
   Widget build(BuildContext context) {
     final _height = MediaQuery.of(context).size.height;
     final _width = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        Expanded(
-          child: SizedBox(
-            height: _height,
-            width: _width,
-            child: ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              itemCount: myPokemonList.length,
-              itemBuilder: (context, index) {
-                return PokemonCard(pokemon: myPokemonList[index]);
-              },
-            ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'My Pokemons',
+          style: TextStyle(
+            color: Colors.black,
           ),
         ),
-      ],
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: _height,
+              width: _width,
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemCount: myPokemonList.length,
+                itemBuilder: (context, index) {
+                  return PokemonCard(pokemon: myPokemonList[index]);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
