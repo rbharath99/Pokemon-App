3d582d8da5131184ca0fab41b1173553fb59447a
Ryan Bharath <dbossman60@gmail.com>
2022-12-01 23:31:27 -0500

    fix: overflow issues on weaknesses chips on mobile

--- lib/pokemon/view/pokemon_details.dart
+++ lib/pokemon/view/pokemon_details.dart
@@ -22,62 +22,86 @@ class PokemonDetailsScreen extends StatelessWidget {
         ),
         backgroundColor: retrievePokemonType(pokemon.type[0]),
       ),
-      body: Container(
-        width: double.infinity,
-        padding: EdgeInsets.only(
-          top: 30,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              pokemon.name,
-              style: TextStyle(
-                fontSize: 20,
+      body: LayoutBuilder(
+        builder: (context, constraints) => Container(
+          width: double.infinity,
+          padding: EdgeInsets.only(
+            top: 30,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                pokemon.name,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
-            ),
-            Image.network(pokemon.image),
-            Text('Height: ${pokemon.height}'),
-            Text('Weight: ${pokemon.weight}'),
-            Text('types:'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: pokemon.type
-                  .map(
-                    (types) => Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 4.0,
+              Image.network(pokemon.image),
+              Text('Height: ${pokemon.height}'),
+              Text('Weight: ${pokemon.weight}'),
+              Text('types:'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: pokemon.type
+                    .map(
+                      (types) => Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 4.0,
+                        ),
+                        child: FilterChip(
+                          pressElevation: 2.0,
+                          backgroundColor: retrievePokemonType(types),
+                          label: Text(types),
+                          onSelected: (_) {},
+                        ),
                       ),
-                      child: FilterChip(
-                        pressElevation: 2.0,
-                        backgroundColor: retrievePokemonType(types),
-                        label: Text(types),
-                        onSelected: (_) {},
-                      ),
-                    ),
-                  )
-                  .toList(),
-            ),
-            Text('weaknesses:'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: pokemon.weaknesses
-                  .map(
-                    (weaknesses) => Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 4.0,
-                      ),
-                      child: FilterChip(
-                        pressElevation: 2.0,
-                        backgroundColor: retrievePokemonType(weaknesses),
-                        label: Text(weaknesses),
-                        onSelected: (_) {},
-                      ),
-                    ),
-                  )
-                  .toList(),
-            ),
-          ],
+                    )
+                    .toList(),
+              ),
+              Text('weaknesses:'),
+              if (constraints.maxWidth < 450)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: pokemon.weaknesses
+                      .map(
+                        (weaknesses) => Expanded(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 4.0,
+                            ),
+                            child: FilterChip(
+                              pressElevation: 2.0,
+                              backgroundColor: retrievePokemonType(weaknesses),
+                              label: Text(weaknesses),
+                              onSelected: (_) {},
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                )
+              else
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: pokemon.weaknesses
+                      .map(
+                        (weaknesses) => Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 4.0,
+                          ),
+                          child: FilterChip(
+                            pressElevation: 2.0,
+                            backgroundColor: retrievePokemonType(weaknesses),
+                            label: Text(weaknesses),
+                            onSelected: (_) {},
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+            ],
+          ),
         ),
       ),
     );
